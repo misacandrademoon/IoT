b5504a90108977ad8119b8d305c07197a984d4f9
IsraAlmaraz <147013951+IsraAlmaraz@users.noreply.github.com>
2023-10-11 19:15:03 -0600

    Update pg1_20231011.py

--- pg1_20231011.py
+++ pg1_20231011.py
@@ -7,7 +7,13 @@ Resp=input('Ingrese color: ')
 opcPar=input('Parpadear SI o NO:')
 
 opc={'Verde':LED(17),'ROJO':LED(18),'AZUL':LED(19)}
-ledOp=opc(Resp)
+
+while decision == 0:
+    if Resp in opc:
+        ledOp=opc(Resp)
+        decision = 1
+    else:
+        Resp=input('Color incorrecto, ingrese otro: ')
 
 # self.led_Blue = LED(led_io)
 # self.led_Red = LED(led_io)
